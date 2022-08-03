ee2f188bec87e2774872091d9f0896f3fac454c0
argarciajr <alfredgarcia.au@gmail.com>
2022-08-03 20:42:47 +1000

    Add CourierServiceTests proj together with OfferCodesTests and a copy of Tombo.db as initial commit.

---
+++ CourierServiceTests/OfferCodesTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CourierService;
+using CourierService.Models;
+
+namespace CourierServiceTests
+{
+    [TestClass]
+    public class OfferCodesTests
+    {
+        [TestMethod]
+        public void IsOfferCodeValid_WithInvalidOfferCode()
+        {
+            //Arrange
+            bool valid;
+            string invalidOfferCode = "OfferCode1";
+            OfferCriteria offerCriteria = new OfferCriteria();
+
+            //Act
+            valid = OfferCodes.IsOfferCodeValid(invalidOfferCode, ref offerCriteria);
+
+            //Assert
+            Assert.AreEqual(false, valid);
+        }
+
+        [TestMethod]
+        public void IsOfferCodeValid_WithValidOfferCode()
+        {
+            //Arrange
+            bool valid;
+            string validOfferCode = "OFR001";
+            OfferCriteria offerCriteria = new OfferCriteria();
+
+            //Act
+            valid = OfferCodes.IsOfferCodeValid(validOfferCode, ref offerCriteria);
+
+            //Assert
+            Assert.AreEqual(true, valid);
+
+        }
+
+        [TestMethod]
+        public void IsOfferCodeValid_WhenOfferCodeLeftBlank()
+        {
+            //Arrange
+            bool valid;
+            string validOfferCode = "";
+            OfferCriteria offerCriteria = new OfferCriteria();
+
+            //Act
+            valid = OfferCodes.IsOfferCodeValid(validOfferCode, ref offerCriteria);
+
+            //Assert
+            Assert.AreEqual(false, valid);
+
+        }
+    }
+}
