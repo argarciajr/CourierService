dd8f679f77e8f0b473d97e3a608b2ec157afb265
argarciajr <alfredgarcia.au@gmail.com>
2022-08-03 11:17:54 +1000

    initial commit

---
+++ CourierService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourierService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
