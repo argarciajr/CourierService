21e3afde0191a67cba14574d978aa7d6e65641a7
argarciajr <alfredgarcia.au@gmail.com>
2022-08-03 11:44:49 +1000

    added new model Package

---
+++ CourierService/Models/Package.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CourierService.Models
+{
+    public class Package
+    {
+        public string PackageID { get; set; }
+        public double Weight { get; set; }
+        public double Distance { get; set; }
+        public string OfferCode { get; set; }
+    }
+}
