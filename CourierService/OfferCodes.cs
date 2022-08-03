49800713bb481401f20737cdb8fc1edcf7ddd6ff
argarciajr <alfredgarcia.au@gmail.com>
2022-08-03 12:05:06 +1000

    added static class OfferCodes.

---
+++ CourierService/OfferCodes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using CourierService.Models;
+
+namespace CourierService
+{
+    public static class OfferCodes
+    {
+        public static bool IsOfferCodeValid(string offerCode, ref OfferCriteria offerCriteria)
+        {
+            bool isValid = false;
+            try
+            {
+                using (var context = new OfferCriteriaContext())
+                {
+                    var offer = from oCriteria in context.OfferCriterias
+                                where oCriteria.OfferCode == offerCode && oCriteria.IsOfferActive == true
+                                select oCriteria;
+
+                    if (offer.Count() > 0)
+                    {
+                        isValid = true;
+                        offerCriteria = offer.FirstOrDefault();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+            return isValid;
+        }
+    }
+}
